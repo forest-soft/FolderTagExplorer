60ecc39e1788c2a8efe6c42641cc4a7d54f6dd6b
森林ソフト <kimoto@forest-soft.net>
2022-05-04 23:14:13 +0900

    とりあえず動くところまで

---
+++ DataAccessLibrary/DataAccessLibrary.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using Microsoft.Data.Sqlite;
+using Windows.Storage;
+using System.IO;
+using System.Collections.Generic;
+
+namespace DataAccessLibrary
+{
+    public static class DataAccess
+    {
+        public async static void InitializeDatabase()
+        {
+            await ApplicationData.Current.LocalFolder.CreateFileAsync("sqliteSample.db", CreationCollisionOption.OpenIfExists);
+            string dbpath = Path.Combine(ApplicationData.Current.LocalFolder.Path, "sqliteSample.db");
+            using (SqliteConnection db = new SqliteConnection($"Filename={dbpath}"))
+            {
+                db.Open();
+
+                String tableCommand = "CREATE TABLE IF NOT EXISTS Folder (" +
+                                            "id INTEGER PRIMARY KEY, " +
+                                            "path NVARCHAR(2048) NULL" +
+                                      ")";
+
+                SqliteCommand createTable = new SqliteCommand(tableCommand, db);
+
+                createTable.ExecuteReader();
+            }
+        }
+
+        public static void AddData(string inputText)
+        {
+            string dbpath = Path.Combine(ApplicationData.Current.LocalFolder.Path, "sqliteSample.db");
+            using (SqliteConnection db = new SqliteConnection($"Filename={dbpath}"))
+            {
+                db.Open();
+
+                SqliteCommand insertCommand = new SqliteCommand();
+                insertCommand.Connection = db;
+
+                // Use parameterized query to prevent SQL injection attacks
+                insertCommand.CommandText = "INSERT INTO Folder (id, path) VALUES (NULL, @Entry);";
+                insertCommand.Parameters.AddWithValue("@Entry", inputText);
+
+                insertCommand.ExecuteReader();
+
+                db.Close();
+            }
+
+        }
+
+        public static List<Dictionary<String, String>> SelectAllData()
+        {
+            List<Dictionary<String, String>> entries = new List<Dictionary<String, String>>();
+
+            string dbpath = Path.Combine(ApplicationData.Current.LocalFolder.Path, "sqliteSample.db");
+            using (SqliteConnection db = new SqliteConnection($"Filename={dbpath}"))
+            {
+                db.Open();
+
+                SqliteCommand sql_command = new SqliteCommand();
+                sql_command.Connection = db;
+
+                // Use parameterized query to prevent SQL injection attacks
+                sql_command.CommandText = "SELECT * FROM Folder ORDER BY id ASC;";
+
+                SqliteDataReader query = sql_command.ExecuteReader();
+
+                while (query.Read())
+                {
+                    Dictionary<String, String> data = new Dictionary<String, String>();
+                    data["id"] = query["id"].ToString();
+                    data["path"] = query["path"].ToString();
+                    // entries.Add(query.GetString(0));
+                    entries.Add(data);
+                }
+
+                db.Close();
+            }
+
+            return entries;
+        }
+
+
+    }
+}
--- FolderTagExplorer/MainPage.xaml.cs
+++ FolderTagExplorer/MainPage.xaml.cs
@@ -1,17 +1,29 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
+using Windows.ApplicationModel;
+using Windows.ApplicationModel.DataTransfer;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.Storage;
+using Windows.Storage.Search;
+using Windows.System;
+using Windows.UI;
+using Windows.UI.Popups;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
 using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Imaging;
 using Windows.UI.Xaml.Navigation;
+using DataAccessLibrary;
+using Windows.Storage.FileProperties;
 
 // 空白ページの項目テンプレートについては、https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x411 を参照してください
 
@@ -22,9 +34,198 @@ namespace FolderTagExplorer
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        private ObservableCollection<NamedColor> recordings = new ObservableCollection<NamedColor>();
+
         public MainPage()
         {
             this.InitializeComponent();
+
+            this.init();
+        }
+
+        private async void init()
+        {
+            // SQLiteのDBに初期データを流し込む。
+            DataAccess.InitializeDatabase();
+
+            List<Dictionary<String, String>> list = DataAccess.SelectAllData();
+            foreach (var v in list)
+            {
+                await this.addItem(v["path"], true);
+            }
+        }
+
+        private void Page_DragOver(object sender, DragEventArgs e)
+        {
+            e.AcceptedOperation = DataPackageOperation.Link;
+            e.DragUIOverride.Caption = "インポート";
+        }
+
+        private async void Page_Drop(object sender, DragEventArgs e)
+        {
+            if (e.DataView.Contains(StandardDataFormats.StorageItems))
+            {
+                var items = await e.DataView.GetStorageItemsAsync();
+                if (items.Count > 0)
+                {
+                    foreach (var item in items)
+                    {
+                        if (item is StorageFile)
+                        {
+                            await this.addItem(((StorageFile)item).Path);
+                        }
+                        else if (item is StorageFolder)
+                        {
+                            await this.addItem(((StorageFolder)item).Path);
+                        }
+                        else
+                        {
+                            MessageDialog md = new MessageDialog("アクセスできませんでした。");
+                            await md.ShowAsync();
+                            return;
+                        }
+                    }
+                }
+            }
+        }
+
+        private void GridView_DataContextChanged(FrameworkElement sender, DataContextChangedEventArgs e)
+        {
+            //null;
+            // 要素追加時に追加した要素の位置までスクロールさせたい。
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            //this.addItem("c:\\ddd");
+            // DataAccess.AddData("button_click");
+        }
+
+        private async Task addItem(string Path, bool is_init = false)
+        {
+            StorageFile storageFile = null;
+            StorageFolder storageFolder = null;
+            Boolean IsFile = false;
+            string name = null;
+            try
+            {
+                storageFile = await StorageFile.GetFileFromPathAsync(Path);
+                IsFile = true;
+                name = storageFile.Name;
+            }
+            catch (Exception e)
+            {
+                try
+                {
+                    storageFolder = await StorageFolder.GetFolderFromPathAsync(Path);
+                    name = storageFolder.Name;
+                }
+                catch (Exception e2)
+                {
+                    MessageDialog md = new MessageDialog("アクセスできませんでした。");
+                    await md.ShowAsync();
+                    return;
+
+                }
+            }
+
+            StorageFile imageFile = null;
+            if (storageFile == null)
+            {
+                QueryOptions options = new QueryOptions();
+                options.FolderDepth = FolderDepth.Shallow;
+                // options.FolderDepth = FolderDepth.Deep;
+                options.FileTypeFilter.Add(".jpg");
+                options.FileTypeFilter.Add(".jpeg");
+                options.FileTypeFilter.Add(".png");
+                options.FileTypeFilter.Add(".gif");
+                /*
+                ネットワークドライブだとソート指定ができない。
+                SortEntry sortOrder = new SortEntry();
+                sortOrder.AscendingOrder = true;
+                sortOrder.PropertyName = "System.FileName";
+                options.SortOrder.Add(sortOrder);
+                */
+
+
+                var result = storageFolder.CreateFileQueryWithOptions(options);
+                IReadOnlyList<StorageFile> fileList = await result.GetFilesAsync(0, 1);
+                if (fileList.Count != 0)
+                {
+                    storageFile = fileList[0];
+                }
+            }
+
+            if (storageFile != null && storageFile.ContentType.StartsWith("image/"))
+            {
+                imageFile = storageFile;
+            }
+            else
+            {
+                imageFile = await StorageFile.GetFileFromApplicationUriAsync(new Uri("ms-appx:///Mochi.png"));
+            }
+
+            var bitmapImage = new BitmapImage();
+            var thumbnail = await imageFile.GetThumbnailAsync(ThumbnailMode.SingleItem, 600);
+            if (thumbnail == null || thumbnail.Type == ThumbnailType.Icon)
+            {
+                // サムネイルのキャッシュが作られてない場合はデフォルトの画像アイコンが返ってきてしまうので、
+                // その場合はオリジナル画像をそのまま表示する。
+                bitmapImage.SetSource(await imageFile.OpenReadAsync());
+            }
+            else
+            {
+                bitmapImage.SetSource(thumbnail);
+            }
+
+            this.recordings.Add(new NamedColor(Path, IsFile, name, bitmapImage));
+
+            if (!is_init)
+            {
+                DataAccess.AddData(Path);
+            }
+        }
+
+        private async void ImageGridView_ItemClick(object sender, ItemClickEventArgs e)
+        {
+            NamedColor ClickedItem = (NamedColor)e.ClickedItem;
+            if (ClickedItem.IsFile)
+            {
+                StorageFile storageFile = await StorageFile.GetFileFromPathAsync(ClickedItem.Path);
+                Boolean result = await Launcher.LaunchFileAsync(storageFile);
+                if (!result)
+                {
+                    // Windowsの設定で既定のアプリが選ばれていないとファイルの起動に失敗するので、アプリ選択画面を表示させる。
+                    var options = new LauncherOptions();
+                    options.DisplayApplicationPicker = true;
+                    await Launcher.LaunchFileAsync(storageFile, options);
+                }
+            }
+            else
+            {
+                StorageFolder storageFolder = await StorageFolder.GetFolderFromPathAsync(ClickedItem.Path);
+                await Launcher.LaunchFolderAsync(storageFolder);
+            }
         }
     }
+
+    class NamedColor
+    {
+        public NamedColor(string Path, Boolean IsFile, string DisplayName, BitmapImage Image)
+        {
+            this.Path = Path;
+            this.IsFile = IsFile;
+            this.DisplayName = DisplayName;
+            this.ImageSource = Image;
+        }
+
+        public string Path { get; set; }
+
+        public Boolean IsFile { get; set; }
+
+        public string DisplayName { get; set; }
+
+        public BitmapImage ImageSource { get; set; }
+    }
+
 }
